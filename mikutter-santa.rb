4babb429d8ed0f0229e137baa775dbcfee1c963b
Masataka Kuwabara <p.ck.t22@gmail.com>
2015-12-26 14:21:40 +0900

    remove debug code

--- mikutter-santa.rb
+++ mikutter-santa.rb
@@ -7,7 +7,6 @@ if (Date.new(today.year, 12, 20)..Date.new(today.year, 12, 31)).cover?(today)
       alias_method :__get_orig, :get
 
       def get(*args)
-        p args
         if args[0] == 'icon.png'
           return File.expand_path("../icon.png", __FILE__)
         end
